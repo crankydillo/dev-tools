78e0c033215321439904e52c8d1743b1861a335f
U-AUSTIN\scox <scox@SCOX-PROD1.aus.pervasive.com>
2012-03-12 22:21:03 -0500

    We have a big list.

--- devtools.hs
+++ devtools.hs
@@ -1,26 +1,37 @@
+import Control.Monad
+import Control.Monad.Trans
 import Prelude hiding (lookup)
 import Data.List hiding (lookup)
-import Data.Map hiding (filter, map)
 import System.Directory
 import System.Environment
 
+main = do 
+        args <- getArgs
+        res <- recurseDirs $ head args
+        print res
+
+recurseDirs :: FilePath -> IO [FilePath]
+recurseDirs d = do
+                 dirs <- listDirs d
+                 subdirs <- mapM recurseDirs dirs
+                 return $ dirs ++ foldl (++) [] subdirs
+
+listDirs :: FilePath -> IO [FilePath]
 listDirs d = do 
-              isDir <- doesDirectoryExist d
-              if isDir
-                  then do
-                         dir <- getDirectoryContents d
-                         print $ show $ listDirs' dir
-                  else print "nope"
+              files <- getDirectoryContents d
+              let pruned = delete "." $ delete ".." files
+              let paths = map ((d ++ "/") ++) pruned
+              dirs <- filterM doesDirectoryExist paths
+              return dirs
 
-listDirs' :: [FilePath] -> [FilePath]
-listDirs' d = ["a"]
-
+{-
 main = do
        args <- getArgs
        let className = head args -- handle empty list
        classTemplate <- readFile "scala-template"
        writeFile "out" $ fillTemplate classTemplate className
        putStrLn className
+-}
        {-
        let m = case lookup arg myMap of
                Just a -> a
@@ -28,7 +39,7 @@ main = do
        putStrLn m
        -}
 
-myMap = fromList [("{class}", msg)]
+--myMap = fromList [("{class}", msg)]
 
 msg = "his"
 
